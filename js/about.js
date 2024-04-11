fb7e7d36b3b3aa0e66e16eb4daa2cb57ef5de76b
DARSHIKA91 <darshikaagrawal9175@gmail.com>
2024-04-11 16:49:09 +0530

    add light dark theam

--- js/about.js
+++ js/about.js
@@ -10,3 +10,13 @@ function vision() {
     const element = document.getElementById("tab-content")
     element.innerText = "  Our work aims to break the vicious cycle of poverty and social isolation and to restore hope for a better future. We believe that every person has the right to access resources and opportunities in order to live and develop with dignity and to become an active and contributing member of our society."
 }
+
+function lightmode() {
+    const theme = document.getElementById("body");
+    theme.className = "theme-light";
+  }
+  
+  function darkmode() {
+    const theme = document.getElementById("body");
+    theme.className = "theme-dark";
+  }
