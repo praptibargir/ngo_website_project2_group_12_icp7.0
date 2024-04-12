6efa2b0759614ea3af788b6ce9d61782cc835917
Aditi-Kundnani <aditikundnani2@gmail.com>
2024-04-12 16:02:29 +0530

    form changed

--- js/volunteer.js
+++ js/volunteer.js
@@ -8,26 +8,50 @@ function lightmode() {
     theme.className = "theme-dark";
   }
   
+  // let currentSlide = 0;
+  //   const slides = document.querySelectorAll('.slide');
+
+  //   function showSlide(n) {
+  //       slides.forEach(slide => {
+  //           slide.style.display = 'none';
+  //       });
+  //       slides[n].style.display = 'block';
+  //   }
+
+  //   function nextSlide() {
+  //       currentSlide = (currentSlide + 1) % slides.length;
+  //       showSlide(currentSlide);
+  //   }
+
+  //   // Initial slide display
+  //   showSlide(currentSlide);
+
+  //   // Automatic slideshow
+  //   setInterval(nextSlide, 2000); // Change slides every 3 seconds
+
+  
   let currentSlide = 0;
-    const slides = document.querySelectorAll('.slide');
+  const slides = document.querySelectorAll('.slide');
 
-    function showSlide(n) {
-        slides.forEach(slide => {
-            slide.style.display = 'none';
-        });
-        slides[n].style.display = 'block';
-    }
+  function showSlide(n) {
+      slides.forEach(slide => {
+          slide.style.display = 'none';
+      });
+      slides[n].style.display = 'block';
+  }
 
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }
+  function nextSlide() {
+      currentSlide = (currentSlide + 1) % slides.length;
+      showSlide(currentSlide);
+  }
 
-    // Initial slide display
-    showSlide(currentSlide);
+  // Initial slide display
+  
+  showSlide(currentSlide);
 
-    // Automatic slideshow
-    setInterval(nextSlide, 2000); // Change slides every 3 seconds
+  // Automatic slideshow
+  setInterval(nextSlide, 2000);
+
 
     function expandCard(cardId) {
       const card = document.getElementById(cardId);
