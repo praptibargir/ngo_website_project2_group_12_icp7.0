d4a4ccb4ad345a9fd5919bd267e70367a3cf187c
praptibargir <praptibargir21@gmail.com>
2024-04-14 19:48:03 +0530

    remove extra js

--- js/faq.js
+++ js/faq.js
@@ -22,14 +22,3 @@ function reset(){
     const theme = document.getElementById("body");
     theme.className = "theme-dark";
   }
-
-  function lightmode() {
-    const theme = document.getElementById("body");
-    theme.className = "theme-light";
-  }
-  
-  function darkmode() {
-    const theme = document.getElementById("body");
-    theme.className = "theme-dark";
-  }
-  
